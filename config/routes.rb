76c147af74a5e742c6026d47884ef5dcd85e5caa
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-22 16:53:29 +0430

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'users', to:'users#index'
-  get 'users/:id', to:'users#show', as: 'user'
-  get 'users/:user_id/posts/:id', to:'posts#show', as: 'user_post'
-  get 'users/:user_id/posts', to:'posts#index', as: 'user_posts'
+  resources :users do
+    resources :posts
+  end
 end
