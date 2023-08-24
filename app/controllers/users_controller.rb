dc18e0cdc555187e137458468920f5ed0a86e7ff
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-24 23:23:20 +0430

    Navigation system added between pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id:1)
+    @user = User.find(params[:id])
     @recent_posts = @user.most_recent
   end
 end
