2f775ee4a3cf681fad58a659ea0b84565134ba63
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-26 15:09:18 +0430

    Controller and views for post,like,comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,34 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
     @comments = @post.comments
+  end
+
+  def new
+    @new_post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = current_user.posts.new(post_params)
+
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      flash.now[:error] = 'Failed to create the post.'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  # filter the paramaters
+  def post_param
+    params.require(:post).permit(:title, :text)
   end
 end
