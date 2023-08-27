0bd7b017acd933ea3619f0622901704ca3f26130
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-08-27 21:46:16 +0430

    Default value added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_150354) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_170802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,8 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_150354) do
     t.string "text"
     t.date "createdAt"
     t.date "updatedAt"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
