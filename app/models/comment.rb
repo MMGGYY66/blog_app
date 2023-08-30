7b174765a8155cfbce56ee89d983aee4fdb69a84
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 11:53:36 +0300

    adding counter_cache: true to the belongs_to :post association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :post
+  # use eager loading to prevent N+1 query problems:
+  # By adding counter_cache: true to the belongs_to :post association
+  belongs_to :post, counter_cache: true
+
   after_create :update_comment_counter
 
   def update_comment_counter
