44eed2ef49b3a25fd7d19de0b46977961363be7f
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 11:55:57 +0300

    make the update_comment_counter callback more efficient by using the update_counters method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,9 @@ class Comment < ApplicationRecord
 
   after_create :update_comment_counter
 
+  # make the update_comment_counter callback more efficient
+  # by using the update_counters method
   def update_comment_counter
-    post.increment!(:comments_counter)
+    Post.update_counters(post.id, comments_counter: 1)
   end
 end
