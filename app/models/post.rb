5a043debcf367d5bab66c75f27edd913d411e74d
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 11:35:26 +0300

    recent_comments method can be further optimized using eager loading to fetch the associated user and to prevent N+1 queries

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   # update the belongs_to :user association with the counter_cache option:
   belongs_to :user, foreign_key: :author_id, counter_cache: true
   # use eager loading to prevent N+1 query problems
+  # By using includes(:user) in the has_many :comments association, 
+  # you're eager loading the associated user for each comment. 
+  # This will help prevent N+1 queries when accessing the comments and 
+  # their associated users.
   has_many :comments, -> { includes(:user) }, dependent: :destroy  
   has_many :likes, dependent: :destroy
 
@@ -10,9 +14,11 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_post_counter
-  # return the 5 most recent comments for a given post.
+
+  # recent_comments method can be further optimized using eager loading 
+  # to fetch the associated user and to prevent N+1 queries
   def self.recent_comments(post, limit = 5)
-    post.comments.order(created_at: :desc).limit(limit)
+    post.comments.includes(:user).order(created_at: :desc).limit(limit)
   end
 
   # updates the posts counter for a user.
