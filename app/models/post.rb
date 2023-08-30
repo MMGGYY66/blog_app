7a686d370771a4e7fd6894d02b130c003ace7546
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 11:36:23 +0300

    Fix linters issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,11 @@ class Post < ApplicationRecord
   # update the belongs_to :user association with the counter_cache option:
   belongs_to :user, foreign_key: :author_id, counter_cache: true
   # use eager loading to prevent N+1 query problems
-  # By using includes(:user) in the has_many :comments association, 
-  # you're eager loading the associated user for each comment. 
-  # This will help prevent N+1 queries when accessing the comments and 
+  # By using includes(:user) in the has_many :comments association,
+  # you're eager loading the associated user for each comment.
+  # This will help prevent N+1 queries when accessing the comments and
   # their associated users.
-  has_many :comments, -> { includes(:user) }, dependent: :destroy  
+  has_many :comments, -> { includes(:user) }, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
@@ -15,7 +15,7 @@ class Post < ApplicationRecord
 
   after_create :update_post_counter
 
-  # recent_comments method can be further optimized using eager loading 
+  # recent_comments method can be further optimized using eager loading
   # to fetch the associated user and to prevent N+1 queries
   def self.recent_comments(post, limit = 5)
     post.comments.includes(:user).order(created_at: :desc).limit(limit)
