814d741d53aa6c81bd5b76fa07ad78df18895e88
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 12:41:57 +0300

    redirecting to the previous page with the appropriate error message

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,7 +6,10 @@ class LikesController < ApplicationController
       redirect_to user_post_path(params[:user_id], params[:post_id])
     else
       flash.now[:error] = 'Failed to like the post'
-      render user_post_path(params[:user_id], params[:post_id])
+      # Instead of rendering the user_post_path in case of an error,
+      # you might want to consider redirecting to the previous page
+      # with the appropriate error message
+      redirect_back(fallback_location: root_path)
     end
   end
 end
