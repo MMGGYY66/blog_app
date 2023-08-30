734f8d68b8113a6ad70f1eb6f1dfc244de79483a
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 12:49:22 +0300

    in comments controller create method, redirecting to the previous page with the appropriate error message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,10 @@ class CommentsController < ApplicationController
       redirect_to user_post_path(params[:user_id], params[:post_id]), notice: 'Comment was successfully created.'
     else
       flash.now[:error] = 'Failed to create the comment.'
-      render :new
+      # instead of rendering the new view in case of an error, you might
+      # want to consider redirecting back to the previous page with
+      # the appropriate error message.
+      redirect_back(fallback_location: root_path)
     end
   end
 
