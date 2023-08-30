0e56ed12e3ed013e7ada6875b2bbe038e305a257
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-30 12:11:50 +0300

    inside users controller index method, using .includes(:posts), you're eager loading the associated posts for all users, which helps prevent N+1 query issues when you access @user.posts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
+  # using .includes(:posts), you're eager loading the associated
+  # posts for all users, which helps prevent N+1 query issues when
+  # you access @user.posts.
   def index
-    @users = User.all
+    @users = User.all.includes(:posts) # Eager load associated posts
   end
 
   def show
